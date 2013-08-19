58d3a245688889d95f93eac43c3ad303f56fd17e
ccaaatt <golanguage@sina.com>
2013-08-19 14:42:13 +0800

    support mobileye +

--- python/qtvid/qtvid.py
+++ python/qtvid/qtvid.py
@@ -1,12 +1,78 @@
 import sys
 import time
 from PySide import QtCore, QtGui
+import serial
+from time import gmtime, strftime
+
+import os
+import binascii
 
 import vid93
-from pythonconsolewidget import PythonConsoleWidget 
+from pythonconsolewidget import PythonConsoleWidget
+
+mytimetmp = strftime("%Y%m%dT%H%M%S", gmtime())
+myport = 'COM6'
+
+j1939_FLI1_list = ['00 00 00 00 00 00 00 00', '10 00 00 00 00 00 00 00',
+'40 00 00 00 00 00 00 00', '00 01 00 00 00 00 00 00', '00 00 00 01 00 00 00 00',
+'f0 00 00 00 00 00 00 00']
+j1939_FLI2_list = ['00 00 00 01 00 00 00 00', '00 00 00 09 00 00 00 00',
+'00 00 00 11 00 00 00 00', '00 00 00 81 00 00 00 00', '00 00 00 00 00 00 00 00']
+j1939_ACC1_list = ['00 00 00 00 00 00 00 00', '00 00 00 00 00 00 40 00','00 40 00 00 00 00 00 00']
+
+
+class ByteSettingSwitch(QtGui.QWidget):
+    def __init__(self, text=None, parent=None):
+        self.name = text
+        if isinstance(text, QtGui.QWidget):
+            parent = text
+            text = None
+        QtGui.QWidget.__init__(self, parent)
+        self.init(text)
+        if text:
+            self.setText(text)
+
+    def init(self, name):
+        self.label = QtGui.QLabel()
+        #self.label.setText(name)
+        ###
+        list_name = name + '_' + 'list'
+        index1 = 0
+        button_group = QtGui.QButtonGroup(self)
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.label)
+        for item in eval(list_name):
+
+            byte_str1 = QtGui.QLineEdit()
+            byte_str1.setText(item)
+            radio = QtGui.QRadioButton(name + str(index1))
+            button_group.addButton(radio)
+            layout.addWidget(radio)
+            layout.addWidget(byte_str1)
+
+            #radio.clicked.connect[QtGui.QAbstractButton](self.setValue(byte_str1.text()))
+            index1 += 1
+        self.setLayout(layout)
+        button_group.buttonClicked[QtGui.QAbstractButton].connect(self.setValue)
+
+        self.setLayout(layout)
+
+    def setValue(self, button):
+        str1 = button.text()
+        name = str1[:-1]
+        index = int(str1[-1])
+        eval('vid93.' + name)['value'] = eval(name + '_list')[index]
+
+    def text(self):
+        return self.label.text()
+
+    def setText(self, text):
+        self.label.setText(text)
+
+
 class LCDRange(QtGui.QWidget):
-    def __init__(self, text=None, range=1000,parent=None):
-        self.name=text
+    def __init__(self, text=None, range=1000, parent=None):
+        self.name = text
         if isinstance(text, QtGui.QWidget):
             parent = text
             text = None
@@ -18,7 +84,7 @@ class LCDRange(QtGui.QWidget):
         if text:
             self.setText(text)
 
-    def init(self,range):
+    def init(self, range):
         lcd = QtGui.QLCDNumber(4)
         self.slider = QtGui.QSlider(QtCore.Qt.Horizontal)
         self.slider.setRange(0, range)
@@ -47,9 +113,15 @@ class LCDRange(QtGui.QWidget):
     def setValue(self, value):
         self.slider.setValue(value)
         if (self.name == 'speed'):
-                vid93.set_speed(value)
-        elif (self.name == 'speed'):
-                 vid93.set_rpm(value)
+            vid93.set_speed(value)
+        elif (self.name == 'rpm'):
+            vid93.set_rpm(value)
+        elif(self.name == 'engineLoad'):
+            vid93.set_engineLoad(value)
+        elif(self.name == 'throttlePosition'):
+            vid93.set_throttlePosition(value)
+        elif(self.name == 'coolantTemperature'):
+            vid93.set_coolantTemperature(value)
 
     def text(self):
         return self.label.text()
@@ -58,7 +130,7 @@ class LCDRange(QtGui.QWidget):
         #~ if minValue < 0 or maxValue > 99 or minValue > maxValue:
             #~ QtCore.qWarning("LCDRange::setRange(%d, %d)\n"
                     #~ "\tRange must be 0..99\n"
-                    #~ "\tand minValue must not be greater than maxValue" % (minValue, maxValue))
+#~ "\tand minValue must not be greater than maxValue" % (minValue, maxValue))
             #~ return
 
         self.slider.setRange(minValue, maxValue)
@@ -66,8 +138,8 @@ class LCDRange(QtGui.QWidget):
     def setText(self, text):
         self.label.setText(text)
 
-class WorkerThread(QtCore.QThread):
 
+class WorkerThread(QtCore.QThread):
 
     def __init__(self, parent=None):
         QtCore.QThread.__init__(self, parent)
@@ -76,10 +148,12 @@ class WorkerThread(QtCore.QThread):
         self.condition = QtCore.QWaitCondition()
         self.restart = False
         self.abort = False
+        self.vidport = "COM1"
 
     def stop(self):
         self.mutex.lock()
         self.abort = True
+        self.alive = False
         self.condition.wakeOne()
         self.mutex.unlock()
 
@@ -87,7 +161,7 @@ class WorkerThread(QtCore.QThread):
 
     def worker(self):
         locker = QtCore.QMutexLocker(self.mutex)
-
+        self.vidport = "COM6"
 
         if not self.isRunning():
             self.start(QtCore.QThread.LowPriority)
@@ -95,34 +169,157 @@ class WorkerThread(QtCore.QThread):
             self.restart = True
             self.condition.wakeOne()
 
+    def createrespond(self, cmd, data, protocol, loghandle):
+                if cmd == b'\x80':
+                    #ping
+                    respone = b'\xaa\x80\x01\x02'
+                elif cmd == b'\x81':
+                    #Perform OBD Protocol Search
+                    if protocol == "j1708":
+                        respone = b'\xaa\x81\x03\x14\x01\x01'
+                    elif protocol == 'j1939':
+                        respone = b'\xaa\x81\x03\x15\x01\x01'
+                    else:
+                        respone = b'\xaa\x81\x03\x11\x01\x01'
+                elif cmd == b'\x84':
+                    #Get VIN
+                    respone = b'\xaa\x84\x09\x31\x32\x33\x34\x35\x36\x37\x38\x39'
+                elif cmd == b'\x90':
+                    #Get VID Application Version
+                    respone = b'\xaa\x90\x02\x11\x11'
+                elif cmd == b'\x91':
+                    #Get Vehicle Battery Voltage
+                    respone = b'\xaa\x91\x02\x0c\x0c'
+                elif cmd == b'\x96':
+                    #Get Bootloader Version/Type
+                    #continue
+                    respone = b'\xaa\x96\x01\x00'
+                elif cmd == b'\x97':
+                    #Get Bootloader Version/Type
+                    respone = b'\xaa\x97\x01\x00'
+                elif cmd == b'\xf0':
+                    #Get Bootloader Version/Type
+                    respone = b'\xaa\xf0\x01\x31'
+                elif cmd == b'\xf4':
+                    #Set Read/Write Address
+                    respone = b'\xaa\xf4\x00'
+                elif cmd == b'\xf8':
+                    #Read Data from EEPROM
+                    respone = b'\xaa\xf8\x20\x31\x32\x33\x34\x35\x36\x37\x38\x31\x00\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+                    respone = b'\xaa\xf8\x20\x4f\x44\x41\x36\x41\x45\x31\x34\x30\x00\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+                    #respone = b'\xaa\xf8\x0f\x31\x32\x33\x34\x35\x36\x37\x38\x39\x30\x31\x32\x33\x34\x35'
+                    #respone = b'\xaa\xf8\x20\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x30\x31'
+                elif cmd == b'\xfe':
+                    #Launch Bootloader (start prog.)
+                    respone = b'\xaa\xfe\x00'
+                elif cmd == b'\xff':
+                    #Launch Application (end prog.)
+                    respone = b'\xaa\xff\x00'
+                elif cmd == b'\x93':
+                    print(data[0])
+                    if data[0] == 3:
+                        dataset = 'fast'
+
+                    elif data[0] == 2:
+                        dataset = 'slow'
+
+                    elif data[0] == 4:
+                        dataset = 'molileye'
+                    respone = bytes.fromhex(vid93.dataset_age(protocol, dataset))
+                else :
+                    return "xxxx"
+
+                summ = 0
+                for byt in respone:
+                    summ = summ + byt
+                summ = summ % 256
+                ddd = bytearray(1)
+                ddd[0] = summ
+                all_respone = respone + ddd
+                print('respond: ', end='')
+                tmpstring = ""
+                for bb in all_respone:
+                    print('%02x ' % bb, end='')
+                    ttt = hex(bb)
+                    tmpstring = tmpstring + " " + ttt
+                loghandle.write(tmpstring + "\n")
+                return all_respone
+
     def run(self):
-        while True:
-                self.mutex.lock()
+        logdir = os.path.join("D:\\", "work", "log")
+        logname = "con" + mytimetmp + ".log"
+        logpath = os.path.join(logdir, logname)
+        mylog = open(logpath, "w")
 
-                self.mutex.unlock()
-                
-                time.sleep(1)
-                print(vid93.dataset_age('obdii','fast'))
+        #vidport = "COM6"
+        vidselect = "ext"
 
+        serial_p = serial.Serial(port=myport, timeout=3, baudrate=57600)
+        self.alive = True
+        #repr_mode=0
+        protocol = "j1939"
+        lenth = -1
+        msgcount = 1
+        try:
 
-
-                #if self.restart:
-                #        break
+            while self.alive:
                 if self.abort:
-                        return
-
-
-                #if not self.restart:
-                        #self.emit(QtCore.SIGNAL("renderedImage(const QImage &, double)"), image, scaleFactor)
-                #        pass
-
-
+                    return
                 self.mutex.lock()
-                #if not self.restart:
-                #        self.condition.wait(self.mutex)
-                #self.restart = False
                 self.mutex.unlock()
 
+
+
+                #print(vid93.dataset_age('obdii','fast'))
+                #self.startport()
+                print("")
+                data = serial_p.read(1)
+                if (data == b'\xb5'):
+                    vidselect = "ext"
+                    print("B5:", end='')
+                elif (data == b'\xb6'):
+                    print("B6:", end='')
+                    vidselect = "int"
+                else:
+                    print(data)
+                    continue
+                cmd = serial_p.read(1)
+                print("command:", end='')
+                print(cmd, end=', ')
+                #cmd=data
+                lenth00 = serial_p.read(1)
+                lenth = ord(lenth00)
+                print("len:", end='')
+                print(lenth, end=', ')
+                if lenth > 30:
+                    print("skip")
+                    continue
+                data = serial_p.read(lenth + 1)
+                print("data:", end='')
+                #print(data)
+                for bb in data:
+                    print('%02x ' % bb, end='')
+                if (vidselect == "int"):
+                    continue
+
+                tmpstring = self.createrespond(cmd, data, protocol, mylog)
+                print(tmpstring)
+                if tmpstring !="xxxx":
+                    serial_p.write(tmpstring)
+
+                
+                msgcount = msgcount + 1
+
+            mylog.close()
+
+        except serial.SerialException:
+            print('SerialException')
+            self.alive = False
+            serial_p.close()
+            mylog.close()
+            raise
+
+        return msgcount
 
 
 class MyWidget(QtGui.QWidget):
@@ -136,39 +333,104 @@ class MyWidget(QtGui.QWidget):
         self.connect(quit, QtCore.SIGNAL("clicked()"),
                      QtGui.qApp, QtCore.SLOT("quit()"))
 
-        speed_widget = LCDRange("speed",200)
+        speed_widget = LCDRange("speed", 200)
         speed_widget.setRange(0, 150)
 
-        
-        rpm_widget = LCDRange("rpm",5000)
-        rpm_widget.setRange(0, 3000)
+        rpm_widget = LCDRange("rpm", 6000)
+        rpm_widget.setRange(0, 6000)
+
+        load_widget = LCDRange("engineLoad", 100)
+        load_widget.setRange(0, 100)
+
+        throttle_widget = LCDRange("throttlePosition", 100)
+        throttle_widget.setRange(0, 100)
+
+        tempt_widget = LCDRange("coolantTemperature", 100)
+        tempt_widget.setRange(-40, 100)
+
+        brake_button = QtGui.QPushButton('brake', self)
+        brake_button.setCheckable(True)
+        brake_button.clicked[bool].connect(self.setSwitch)
+
+        clutch_button = QtGui.QPushButton('clutch', self)
+        clutch_button.setCheckable(True)
+        clutch_button.clicked[bool].connect(self.setSwitch)
+
+        console = PythonConsoleWidget()
+
+        middleLayout = QtGui.QVBoxLayout()
+        middleLayout.addWidget(brake_button)
+        middleLayout.addWidget(clutch_button)
+        middleLayout.addWidget(console)
+        fli1_widget = ByteSettingSwitch('j1939_FLI1')
+        middleLayout.addWidget(fli1_widget)
+        fli2_widget = ByteSettingSwitch('j1939_FLI2')
+        middleLayout.addWidget(fli2_widget)
+        acc1_widget = ByteSettingSwitch('j1939_ACC1')
+        middleLayout.addWidget(acc1_widget)
+        availablePort = []
+        for i in range(256):
+                try:
+                        s = serial.Serial(i)
+                        availablePort.append((i, s.portstr))
+                        s.close()
+                        #explicit close 'cause of delayed GC in java
+                except serial.SerialException:
+                    pass
+        for n, s in availablePort:
+            print("(%d) %s" % (n, s))
+
+        self.portBox= QtGui.QComboBox()
 
 
-        console=PythonConsoleWidget()
-        
         shoot = QtGui.QPushButton("&Shoot")
         shoot.setFont(QtGui.QFont("Times", 18, QtGui.QFont.Bold))
         self.connect(shoot, QtCore.SIGNAL("clicked()"), self.thread.worker)
         topLayout = QtGui.QHBoxLayout()
+        self.portBox= QtGui.QComboBox()
+        for n, s in availablePort:
+            self.portBox.addItem(s)
+        self.portBox.setCurrentIndex(0)
+        self.connect(self.portBox, QtCore.SIGNAL("activated()"), self.setPort())
+        topLayout.addWidget(self.portBox)
         topLayout.addWidget(shoot)
         topLayout.addStretch(1)
 
         leftLayout = QtGui.QVBoxLayout()
         leftLayout.addWidget(speed_widget)
         leftLayout.addWidget(rpm_widget)
+        leftLayout.addWidget(load_widget)
+        leftLayout.addWidget(throttle_widget)
+        leftLayout.addWidget(tempt_widget)
 
         gridLayout = QtGui.QGridLayout()
         gridLayout.addWidget(quit, 0, 0)
         gridLayout.addLayout(topLayout, 0, 1)
         gridLayout.addLayout(leftLayout, 1, 0)
-        gridLayout.addWidget(console, 1, 1, 2, 1)
+        gridLayout.addLayout(middleLayout, 1, 1)
+        #gridLayout.addWidget(console, 1, 2, 2, 1)
         gridLayout.setColumnStretch(1, 10)
         self.setLayout(gridLayout)
 
-        speed_widget.setValue(60)
-        rpm_widget.setValue(100)
-        
+        speed_widget.setValue(20)
+        rpm_widget.setValue(500)
+        load_widget.setValue(60)
+        throttle_widget.setValue(70)
+        tempt_widget.setValue(10)
+
         speed_widget.setFocus()
+
+    def setSwitch(self, pressed):
+        source = self.sender()
+        if source.text() == "brake":
+            vid93.swtich('brake', pressed)
+        elif source.text() == "clutch":
+            vid93.swtich('clutch', pressed)
+        else:
+            pass
+    def setPort(self):
+        myport=self.portBox.currentText()
+
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
--- python/qtvid/vid93.py
+++ python/qtvid/vid93.py
@@ -2,38 +2,64 @@ head93='aa 93'
 
 did = {'speed' : '00', 'engineLoad':'02','engineRpm':'04','throttlePosition':'05', \
 'brakePedalStatus':'0b','massAirFlow':'0d','calculatedFuelUsed':'0e','safetyStatus':'11', \
- 'milStatus':'01','coolantTemperature':'03','PTOStatus':'06'}
-
-j1708_speed = {'value': '00',  'uid': '04', }
+ 'milStatus':'01','coolantTemperature':'03','PTOStatus':'06','odometer':'09', \
+  'vehicleBattery':'08', 'fuelLevel':'07', 'totalfuel':'0a', 'engineHours':'0c','FLI1':'13', 'FLI2':'14','ACC1':'15'}
+def int2little(value):
+        bigend=format(value,'04x')
+        return bigend[2:4]+' '+bigend[0:2]
+        
+        
+j1708_speed = {'value': '50',  'uid': '04', }
 j1939_speed = {'value': '00 00', 'uid': '05', }
 obdii_speed = {'value': '00', 'uid': '06', }
 def set_speed(value):
-        j1708_speed['value']=format((value+1)//10,"02x")
-        j1939_speed['value']='00 '+format((value+1),"02x")
-        obdii_speed['value']=format((value+1)//3,"02x")
-        print(j1708_speed)
-        
+        j1708_speed['value']=format(value*8//5,"02x") 
+        j1939_speed['value']=int2little(value*256)
+        obdii_speed['value']=format(value,"02x")
+
+
 j1708_engineLoad = {'value': '5e',  'uid': '0c', }
 j1939_engineLoad = {'value': '20', 'uid': '09', }
 obdii_engineLoad = {'value': '21', 'uid': '0a', }
 
-j1708_engineRpm = {'value': '00 20',  'uid': '07', }
+def set_engineLoad(value):
+        print('set_engineLoad')
+        j1708_engineLoad['value']=format(value*2,"02x") 
+        j1939_engineLoad['value']=format(value,"02x")
+        obdii_engineLoad['value']=format(value*255//100,"02x")
+
+j1708_engineRpm = {'value': '00 00',  'uid': '07', }
 j1939_engineRpm = {'value': '11 11', 'uid': '08', }
 obdii_engineRpm = {'value': 'a0 00', 'uid': '07', }
 
-def set_rpm(rpm):
-        j1708_engineRpm['value']='00 '+format((value+1),"02x")
-        j1939_engineRpm['value']='00 '+format((value+1),"02x")
-        obdii_engineRpm['value']=format((value+1),"02x")+' 00'
+def set_rpm(value):
+        print('set_rpm')
+        j1708_engineRpm['value']=int2little(value*4)
+        j1939_engineRpm['value']=int2little(value*8)
+        obdii_engineRpm['value']=int2little(value*4)
         
 j1708_throttlePosition = {'value': '50',  'uid': '0b', }
 j1939_throttlePosition = {'value': '19', 'uid': '0b', }
-obdii_throttlePosition = {'value': '60', 'uid': '0s', }
+obdii_throttlePosition = {'value': '60', 'uid': '0a', }
+def set_throttlePosition(value):
+        print('set_throttlePosition')
+        j1708_throttlePosition['value']=format(value*5//2,"02x") 
+        j1939_throttlePosition['value']=format(value*5//2,"02x") 
+        obdii_throttlePosition['value']=format(value*255//100,"02x")
 
 j1708_brakePedalStatus = {'value': '00',  'uid': '16', }
 j1939_brakePedalStatus = {'value': '00', 'uid': '19', }
 obdii_brakePedalStatus = {'value': '', 'uid': '00', }
-
+def swtich(which,pressed):
+        print('set ',which)
+        if which == 'brake':
+                if pressed:
+                        j1708_brakePedalStatus['value']='50'
+                        j1939_brakePedalStatus['value']='10'
+                else:
+                        j1708_brakePedalStatus['value']='00'
+                        j1939_brakePedalStatus['value']='00'
+        
 j1708_massAirFlow = {'value': '10 00',  'uid': '1d', }
 j1939_massAirFlow = {'value': '28 00', 'uid': '1e', }
 obdii_massAirFlow = {'value': '64 00', 'uid': '1c', }
@@ -50,26 +76,46 @@ obdii_milStatus = {'value': '80 00 00 80', 'uid': '13', }
 j1708_coolantTemperature = {'value': '40',  'uid': '0e', }
 j1939_coolantTemperature = {'value': '64', 'uid': '0d', }
 obdii_coolantTemperature = {'value': '64', 'uid': '0d', }
+def set_coolantTemperature(value):
+        print('set_coolantTemperature')
+        j1708_coolantTemperature['value']=format((value*9//5+32),"02x") 
+        j1939_coolantTemperature['value']=format((value+40),"02x") 
+        obdii_coolantTemperature['value']=format((value+40),"02x")
+        
 
 j1708_PTOStatus = {'value': '00',  'uid': '17', }
 j1939_PTOStatus = {'value': '80 00 00 00 00 00 00 00', 'uid': '18', }
 obdii_PTOStatus = {'value': '81', 'uid': '14', }
 
-j1708_ = {'value': '',  'uid': '', }
-j1939_ = {'value': '', 'uid': '', }
-obdii_ = {'value': '', 'uid': '', }
+j1708_odometer = {'value': 'ce 22 08 00',  'uid': '01', }
+j1939_odometer = {'value': 'a0 00 00 00', 'uid': '02', }
+obdii_odometer = {'value': '', 'uid': '00', }
 
-j1708_ = {'value': '',  'uid': '', }
-j1939_ = {'value': '', 'uid': '', }
-obdii_ = {'value': '', 'uid': '', }
+j1708_vehicleBattery = {'value': '0a 00',  'uid': '11', }
+j1939_vehicleBattery = {'value': '00 ff 53 00 26 00 13 00', 'uid': '1a', }
+obdii_vehicleBattery = {'value': '00 a0', 'uid': '12', }
+
+j1708_fuelLevel = {'value': '14',  'uid': '0c', }
+j1939_fuelLevel = {'value': '19', 'uid': '0b', }
+obdii_fuelLevel = {'value': '21', 'uid': '0a', }
+
+j1708_totalfuel = {'value': '64 00 00 00',  'uid': '0f', }
+j1939_totalfuel = {'value': '64 00 00 00', 'uid': '10', }
+obdii_totalfuel = {'value': '', 'uid': '00', }
+
+j1708_engineHours = {'value': '10 00 00 00',  'uid': '1b', }
+j1939_engineHours = {'value': '01 02 03 04 01 02 03 04', 'uid': '1f', }
+obdii_engineHours = {'value': '', 'uid': '00', }
+
+j1939_FLI1 = {'value': '00 00 00 00 00 00 00 00', 'uid': '29'}
+j1939_FLI2 = {'value': 'f5 00 00 00 00 00 00 00', 'uid': '2a'}
+j1939_ACC1 = {'value': 'ff ff 00 00 00 00 00 00', 'uid': '2b' }
 
 fast_set = ['speed','engineLoad','engineRpm','throttlePosition','brakePedalStatus','massAirFlow','calculatedFuelUsed','safetyStatus']
-fast_set = ['speed','engineRpm']
-slow_set = ['milStatus','coolantTemperature','PTOStatus']
-molileye_set = []
+slow_set = ['milStatus','coolantTemperature','PTOStatus','odometer','vehicleBattery','fuelLevel', 'totalfuel','engineHours']
+molileye_set = ['FLI1','FLI2','ACC1']
 allset={'fast':fast_set, 'slow':slow_set,'molileye': molileye_set}
 idset={'fast':'03', 'slow':'02','molileye': '04'}
-
 
 def obdcat(*strlist):
                 local_str=''
@@ -88,20 +134,23 @@ def dataset_age(protocol,setname,age='10 00'):
                 element_number=0
                 set=allset[setname]
                 for item in set:
-                        print(item)
+                        #print(item)
                         key=protocol+"_"+item
-                        print(key)
+                        if item == 'speed':
+                                print('speed=',(eval(key))['value'])
+                        #print(key)
 
                         if key in globals():
                                 value_len=format((len((eval(key))['value'])+1)//3,"02x")
                                 return_string=obdcat(return_string,did[item],(eval(key))['uid'],value_len,(eval(key))['value'],age)
                                 element_number=element_number+1
-                print(return_string)
+                #print(return_string)
                 print(len(return_string))
-                byte_len=len(return_string)//3+2
+                byte_len=(len(return_string)+1)//3+2
 
-                return_string=obdcat(head93, format(byte_len,"02x"), format(element_number,"02x"),return_string)
+                return_string=obdcat(head93, format(byte_len,"02x"), idset[setname], format(element_number,"02x"),return_string)
                 return return_string
+
 if __name__ == '__main__':
-        print ("start:")
-        print(dataset_age('obdii','fast'))
+        print(int2little(255*256))
+        print(dataset_age('obdii','slow'))
